a82fc5ccee04bd904ce9e30533a1f38cc3ec4020
Jason Ehrlicher <jehrlich@uno.edu>
2023-10-18 12:04:21 -0500

    Fundamentals, Question 8

---
+++ Fundamentals/index8.js
@@ -0,0 +1,33 @@
+// Question 8:
+// The object below represents a single city.
+// a) Write a function that takes an object as an argument and uses a for...in loop to access
+// and print to the console each of those object properties and their values. Test it using
+// the sydney object below.
+// b) Create a new object for a different city with different properties and call your function
+// again with the new object.
+
+const sydney = {
+    name: 'Sydney',
+    population: 5_121_000,
+    state: 'NSW',
+    founded: '26 January 1788',
+    timezone: 'Australia/Sydney'
+    };
+
+function printObject(object) {
+    for (const key in object) {
+    console.log(`${key}: ${object[key]}`)
+}
+}
+
+printObject(sydney);
+
+const newyork = {
+    name: 'New York',
+    population: 8419600,
+    state: 'NY',
+    founded: '1624',
+    timezone: 'America/New_York'
+  };
+
+  printObject(newyork);
